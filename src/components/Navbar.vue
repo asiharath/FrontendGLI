14a3af9a29da76a1f48175aee48e5c0c7b53a41a
AlexS <alex.siharath@yahoo.com>
2019-11-22 23:36:14 +0100

    :art: add Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar">
+    <router-link to="/"></router-link>
+    <Autocomplete placeholder="Search" :search="search"></Autocomplete>
+  </div>
+</template>
+
+<script>
+import Autocomplete from "@trevoreyre/autocomplete-vue";
+
+export default {
+  name: "navbar",
+  components: {
+    Autocomplete
+  }
+};
+</script>
